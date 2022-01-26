7a52df7d0ee93737667f85b0989b4962f19bde8f
runnerboy22 <runnerboy22@gmail.com>
2022-01-26 11:18:26 -0500

    removed loop - balance doesn't update in real time

--- helium.sh
+++ helium.sh
@@ -2,11 +2,9 @@
 
 balance=`helium-wallet balance | awk 'NR==4{print $4}'`
 
-while true; do
 if (($balance > 10))
 then
     json -I -f wallets.json -e "this.amount=$balance/2-.035"
     helium-wallet pay multi --commit wallets.json
     # helium-wallet pay one --commit 14DJK27rfozqZ5MAwH73ZRHyp9HtBpizc5HoCEEeqFqScPweWff 1
     fi
-done
